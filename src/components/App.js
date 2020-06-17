6d7a6180e36b1ba69f77c60f7411da7e0dcbf058
Victoria Hawoldar <victoria@hawoldar.co.uk>
2020-06-17 10:50:03 +0100

    update formatting

--- src/components/App.js
+++ src/components/App.js
@@ -6,50 +6,50 @@ import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
-	state = {
-		videos: [],
-		selectedVideo: null
-	};
+  state = {
+    videos: [],
+    selectedVideo: null
+  };
 
-	componentDidMount() {
-		this.onFormSubmit('popular videos');
-	}
+  componentDidMount() {
+    this.onFormSubmit('popular videos');
+  }
 
-	onFormSubmit = async (term) => {
-		const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
-			params: {
-				q: term,
-				part: 'snippet',
-				type: 'video',
-				maxResults: 5,
-				key: process.env.REACT_APP_API_KEY
-			}
-		});
+  onFormSubmit = async (term) => {
+    const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+      params: {
+        q: term,
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: process.env.REACT_APP_API_KEY
+      }
+    });
 
-		this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
-	};
+    this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
+  };
 
-	onVideoSelect = (video) => {
-		this.setState({ selectedVideo: video });
-	};
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
 
-	render() {
-		return (
-			<div className="ui container">
-				<SearchBar onFormSubmit={this.onFormSubmit} />
-				<div className="ui grid">
-					<div className="ui row">
-						<div className="eleven wide column">
-							<VideoDetail video={this.state.selectedVideo} />
-						</div>
-						<div className="five wide column">
-							<VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onFormSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -2,19 +2,19 @@ import React from 'react';
 import './videoItem.css';
 
 const VideoItem = ({ video, onVideoSelect }) => {
-	return (
-		<div
-			className="video-item item"
-			onClick={() => {
-				onVideoSelect(video);
-			}}
-		>
-			<img className="ui image" alt={video.snippet.title} src={video.snippet.thumbnails.medium.url} />
-			<div className="content">
-				<div className="header">{video.snippet.title}</div>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className="video-item item"
+      onClick={() => {
+        onVideoSelect(video);
+      }}
+    >
+      <img className="ui image" alt={video.snippet.title} src={video.snippet.thumbnails.medium.url} />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoItem;
